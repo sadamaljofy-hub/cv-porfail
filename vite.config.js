37de8c50ce084ac763116a995cadc3ab008bc744
sadamaljofy-hub <sadamaljofy@gmail.com>
2025-10-17 04:41:53 +0300

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/cv-portfolio/',
+  base: '/cv-porfail/',
   plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
